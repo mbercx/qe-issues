27c14f6a41e31e907a2041a1f17deedb7e48bdda
Marnik Bercx <mbercx@gmail.com>
2023-12-19 23:57:20 +0100

    ✨ Add all iterations for `batch1` failures

---
+++ scf-convergence/batch1/mpds-S1814884/iteration_02/submit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+#SBATCH --no-requeue
+#SBATCH --job-name="aiida-978636"
+#SBATCH --get-user-env
+#SBATCH --output=_scheduler-stdout.txt
+#SBATCH --error=_scheduler-stderr.txt
+#SBATCH --partition=normal
+#SBATCH --account=s1073
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=10:00:00
+
+### computer prepend_text start ###
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+
+module load daint-gpu
+
+if [ $NTASKS_PER_NODE -eq 1 ]; then
+    CRAY_CUDA_MPS=0
+else
+    CRAY_CUDA_MPS=1
+fi
+
+export CRAY_CUDA_MPS=$CRAY_CUDA_MPS
+export MPICH_MAX_THREAD_SAFETY=multiple
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+ulimit -s unlimited
+### computer prepend_text end ###
+
+
+module load QuantumESPRESSO/7.2-CrayNvidia-21.09
+
+
+'srun' '-n' '1' '/apps/daint/UES/jenkins/7.0.UP03/21.09/daint-gpu/software/QuantumESPRESSO/7.2-CrayNvidia-21.09/bin/pw.x' '-npool' '1' '-in' 'aiida.in'  > 'aiida.out'
+
+ 
